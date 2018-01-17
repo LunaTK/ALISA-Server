2e9bde637ed68d2cd6eb2384234aaf58bd0dc78a
TaeGeun Moon <ahn_v3@naver.com>
2018-01-17 13:40:14 +0900

    충돌테스트용

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,7 +3,7 @@ public class MainClass {
 
 	public static void main(String[] args)
 	{
-		System.out.println("hi sangwook");
-		System.out.println("hello sangwook");
+		String test = "asdf";
+	
 	}
 }
